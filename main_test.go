df21221fdf377c65d21a7cb27f1181fb5f62da69
Pedro Charun <mlosail3@gmail.com>
2021-07-02 03:26:08 -0500

    dev: Creado tests para la aplicacion.

--- main_test.go
+++ main_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // TestRotarMatriz2x2: ejecuta una rotacion de matriz de 2x2
@@ -41,4 +46,33 @@ func TestRotarMatriz3x3(t *testing.T) {
 		t.Error("La rotacion de la matriz 3x3 ha tenido errores")
 		t.Fail()
 	}
+}
+
+var application = InitServe()
+
+// TestIndexRoute: Test para la ruta index de la application.
+func TestIndexRoute(t *testing.T) {
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/", nil)
+	application.ServeHTTP(w, request)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// TestRotateMatrixRoute: Test para la ruta de rotacion de matrix
+func TestRotateMatrixRoute(t *testing.T) {
+	w := httptest.NewRecorder()
+	input := "{\"matrix\":[[1,2],[3,4]]}"
+	request, _ := http.NewRequest("POST", "/rotate-matrix/", strings.NewReader(input))
+	application.ServeHTTP(w, request)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// TestRotateMatrixPayloadRoute: Test para el contenido de la ruta rotate matrix
+func TestRotateMatrixPayloadRoute(t *testing.T) {
+	w := httptest.NewRecorder()
+	input := "{\"matrix\":[[1,2],[3,4]]}"
+	output := "{\"matrix\":[[2,4],[1,3]]}"
+	request, _ := http.NewRequest("POST", "/rotate-matrix/", strings.NewReader(input))
+	application.ServeHTTP(w, request)
+	assert.Equal(t, output, w.Body.String())
 }
